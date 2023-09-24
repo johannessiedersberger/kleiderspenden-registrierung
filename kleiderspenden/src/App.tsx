89b32bdd3747d22276c0603e39f7bde87b0acf51
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2023-09-24 16:08:49 +0200

    added zustand store

--- kleiderspenden/src/App.tsx
+++ kleiderspenden/src/App.tsx
@@ -8,6 +8,29 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import { ChakraProvider } from '@chakra-ui/react'
 import Imprint from './components/legalpages/Imprint';
 import FormPage from './components/formpages/FormPage';
+import { persist, devtools } from 'zustand/middleware';
+import create from 'zustand';
+import ConfirmationPage from './components/formpages/ConfirmationPage';
+
+interface DonationState {
+  donation: Object | null,
+  setDonation: (currentDonation: []) => void,
+  removeDonation: () => void
+}
+
+export const useStore = create<DonationState>()(
+  devtools(
+    persist(
+      (set) => ({
+        donation: Object,
+        setDonation: (currentDonation) => set({ donation: currentDonation }),
+        removeDonation: () => set({ donation: null })
+      }), {
+      name: 'donation', // name of the item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+    })
+  )
+);
 
 function App() {
   return (
@@ -17,6 +40,7 @@ function App() {
           <Route path="/" element={<FrontPage />} />
           <Route path="/impressum" element={<Imprint />} />
           <Route path="/spenden" element={<FormPage />} />
+          <Route path="/bestaetigung" element={<ConfirmationPage />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
